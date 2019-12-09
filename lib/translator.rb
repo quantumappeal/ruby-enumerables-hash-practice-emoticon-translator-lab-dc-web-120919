6071859dd68c461a5b767d8e26ad7903c1ca6bd5
Sean Beach <sean.beach1@yahoo.com>
2019-12-09 03:30:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,8 @@ def load_library
 end
 
 def get_japanese_emoticon
-  # code goes here
+   result = load_library(file_path)['get_emoticon'][emoticon]
+  result ? result : "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning
